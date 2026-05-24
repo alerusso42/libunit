fb3edf0d708e628278e0a63e626493024cb73bb1
Alessandro Russo <alerusso@c1r2p4.42firenze.it>
2026-05-24 18:20:32 +0200

    fix norminette

--- bonus/tests/atoi/03_partial_num.c
+++ bonus/tests/atoi/03_partial_num.c
@@ -6,7 +6,7 @@
 /*   By: alerusso <alerusso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 17:22:02 by alerusso          #+#    #+#             */
-/*   Updated: 2026/05/24 18:18:31 by alerusso         ###   ########.fr       */
+/*   Updated: 2026/05/24 18:20:01 by alerusso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int	atoi_test_partial_num(void)
 {
 	char	*s;
 
-	s = "+-+27eoacq; v4t 97c243rwj+-*/<iodvc sw;qap.//'\"\\w3y34ao012';p][db+>]";
+	s = "+-+27eo; v4t 97c243rwj+-*/<iodvc sw;qap.//'\"\\w3y34ao012';p][db+>]";
 	return (-(atoi(s) != 27));
 }
